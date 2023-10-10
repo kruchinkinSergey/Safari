2f09b457f303cae817b3b89be0e0fe2a613c4864
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-10-10 10:57:06 +0300

    safari
    safari

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('.about__video-link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+    $('.gallery__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>'
+    });
+    $('.gallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+});
